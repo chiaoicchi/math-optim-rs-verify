a68be678117c4cc9d3f91d2efc0e00ab2a214d44
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-03-01 19:48:07 +0900

    feat: lis

---
+++ src/bin/longest_increasing_subsequence.rs
@@ -0,0 +1,28 @@
+use seqenz::lis::lis;
+use std::io::{BufWriter, Read, Write, stdin, stdout};
+
+fn main() {
+    let mut input = Vec::new();
+    stdin().lock().read_to_end(&mut input).unwrap();
+    let mut iter = input.split(|&b| b <= b' ').filter(|s| !s.is_empty());
+    let mut stdout = BufWriter::new(stdout().lock());
+
+    macro_rules! parse {
+        ($t:ty) => {{
+            let s = iter.next().unwrap();
+            let mut x: $t = 0;
+            for &b in s {
+                x = x * 10 + (b - b'0') as $t;
+            }
+            x
+        }};
+    }
+
+    let n = parse!(usize);
+    let a: Vec<u32> = (0..n).map(|_| parse!(u32)).collect();
+    let lis = lis(&a);
+    writeln!(stdout, "{}", lis.len()).ok();
+    for i in lis {
+        write!(stdout, "{} t", i).ok();
+    }
+}
